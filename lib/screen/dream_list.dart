dcba45de996043eb754273804ffac24758f211d8
Firrizqia <raflifirrizqiardi@gmail.com>
2025-05-07 23:33:30 +0700

    landasan

---
+++ lib/screen/dream_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DreamList extends StatefulWidget {
+  const DreamList({super.key});
+
+  @override
+  State<DreamList> createState() => _DreamListState();
+}
+
+class _DreamListState extends State<DreamList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
